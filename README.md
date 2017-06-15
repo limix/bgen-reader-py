0b56db974c6c7789a4a5b341708cbfa766be8939
Danilo Horta <danilo.horta@gmail.com>
2017-06-15 08:24:45 +0100

    limix to  bgen-reader

--- README.md
+++ README.md
@@ -1,60 +1,12 @@
-# Limix
+# Bgen-reader
 
-[![PyPI-License](https://img.shields.io/pypi/l/limix.svg?style=flat-square)](https://pypi.python.org/pypi/limix/)
-[![PyPI-Version](https://img.shields.io/pypi/v/limix.svg?style=flat-square)](https://pypi.python.org/pypi/limix/)
-[![Documentation Status](https://readthedocs.org/projects/limix/badge/?style=flat-square&version=latest)](https://limix.readthedocs.io/)
-
-Genomic analyses require flexible models that can be adapted to the needs of the user.
-Limix is a flexible and efficient linear mixed model library with interfaces to Python.
-
-Limix includes methods for
-- single-variant association and interaction testing,
-- variance decompostion analysis with linear mixed models,
-- association and interaction set tests,
-- as well as different utils for statistical analysis, basic i/o and plotting.
-
-A description of the public interface is found at
-https://limix.readthedocs.io/.
-
-iPython notebook tutorials are available from github repository:
-https://github.com/limix/limix-tutorials.
-
-These tutorials can also be viewed using the ipython notebook viewer:
-http://nbviewer.ipython.org/github/limix/limix-tutorials/blob/master/index.ipynb.
-
-#### Highlights
-
-- **iSet**: an interaction set test with external context
-  ([paper](http://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1006693))
-  ([public interface](http://limix.readthedocs.io/iSet.html))
-  ([tutorial](https://github.com/limix/limix-tutorials/tree/master/iSet))
-
-- **mtSet**: an efficient multi-trait set test
-  ([paper](http://www.nature.com/nmeth/journal/v12/n8/abs/nmeth.3439.html))
-  ([public interface](http://limix.readthedocs.io/mtSet.html))
-  ([tutorial](https://github.com/limix/limix-tutorials/tree/master/mtSet))
-
-## Install
-
-The recommended way of installing it is via
-[pip](https://pypi.python.org/pypi/pip)
-
-```bash
-pip install limix
-```
-
-## Problems
-
-If you encounter any issue, please, [submit it](https://github.com/limix/limix/issues).
+If you encounter any issue, please, [submit it](https://github.com/limix/bgen-reader-py/issues).
 
 ## Authors
 
-* **Christoph Lippert** - [https://github.com/clippert](https://github.com/clippert)
-* **Danilo Horta** - [https://github.com/Horta](https://github.com/Horta)
-* **Francesco Paolo Casale** - [https://github.com/fpcasale](https://github.com/fpcasale)
-* **Oliver Stegle** - [https://github.com/ostegle](https://github.com/ostegle)
+* **Danilo Horta** - [https://github.com/Horta](https://github.com/horta)
 
 ## License
 
-This project is licensed under the Apache License (Version 2.0, January 2004) -
-see the [LICENSE](LICENSE) file for details
+This project is licensed under the MIT License - see the
+[LICENSE](LICENSE) file for details
--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 import os
 import sys
 
-
 from setuptools import find_packages, setup
 
 try:
@@ -22,42 +21,21 @@ def setup_package():
     needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
     pytest_runner = ['pytest-runner>=2.9'] if needs_pytest else []
 
-    setup_requires = ["cython", "numpy"] + pytest_runner
-    install_requires = [
-        'scikit-learn', 'limix-core>=1.0.1',
-        'dask[array,bag,dataframe,delayed]>=0.14', 'h5py',
-        'pandas-plink>=1.1.7', 'limix-legacy', 'glimix-core>=1.2.12',
-        'joblib>=0.11', 'tqdm>=4.10', 'scipy>=0.18', 'distributed',
-        'numpy-sugar>=1.0.41', 'ncephes>=1.0.38', 'asciitree>=0.3.3'
-    ]
-    tests_require = ['pytest', 'pytest-console-scripts', 'pytest-pep8']
-
-    console_scripts = [
-        'limix_runner=limix.scripts.limix_runner:entry_point',
-        'mtset_postprocess=limix.scripts.mtset_postprocess:entry_point',
-        'mtset_preprocess=limix.scripts.mtset_preprocess:entry_point',
-        'mtset_definesets=limix.scripts.mtset_definesets:entry_point',
-        'mtset_simpheno=limix.scripts.mtset_simpheno:entry_point',
-        'mtset_analyze=limix.scripts.mtset_analyze:entry_point',
-        'limix_converter=limix.scripts.limix_converter:entry_point',
-        'iset_analyze=limix.scripts.iset_analyze:entry_point',
-        'iset_postprocess=limix.scripts.iset_postprocess:entry_point',
-        'limix=limix.scripts.limix:entry_point',
-        'ilimix=limix.scripts.ilimix:entry_point'
-    ]
+    setup_requires = [] + pytest_runner
+    install_requires = []
+    tests_require = ['pytest', 'pytest-pep8']
 
     metadata = dict(
-        name='limix',
-        version='1.1.0',
-        maintainer="Limix Developers",
+        name='bgen-reader',
+        version='0.1.0',
+        maintainer="Danilo Horta",
         maintainer_email="horta@ebi.ac.uk",
-        author=("Christoph Lippert, Danilo Horta, " +
-                "Francesco Paolo Casale, Oliver Stegle"),
-        author_email="stegle@ebi.ac.uk",
-        license="Apache License 2.0'",
-        description="A flexible and fast mixed model toolbox.",
+        author="Danilo Horta",
+        author_email="horta@ebi.ac.uk",
+        license="MIT",
+        description="Bgen file format reader.",
         long_description=long_description,
-        url='https://github.com/limix/limix',
+        url='https://github.com/limix/bgen-reader-py',
         packages=find_packages(),
         zip_safe=False,
         install_requires=install_requires,
@@ -68,8 +46,7 @@ def setup_package():
             "Development Status :: 5 - Production/Stable",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
-        ],
-        entry_points={'console_scripts': console_scripts})
+        ])
 
     try:
         setup(**metadata)
