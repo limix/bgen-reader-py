8ca257ce985576f45d24229a54b2aa09678976f9
Danilo Horta <danilo.horta@gmail.com>
2017-06-15 08:26:51 +0100

    init

---
+++ bgen_reader/__init__.py
@@ -0,0 +1,39 @@
+"""
+*******************
+bgen_reader package
+*******************
+
+Bgen file format reader.
+
+"""
+
+from __future__ import absolute_import as _absolute_import
+
+from pkg_resources import DistributionNotFound as _DistributionNotFound
+from pkg_resources import get_distribution as _get_distribution
+
+try:
+    __version__ = _get_distribution('bgen_reader').version
+except _DistributionNotFound:
+    __version__ = 'unknown'
+
+
+def test():
+    import os
+    p = __import__('bgen_reader').__path__[0]
+    src_path = os.path.abspath(p)
+    old_path = os.getcwd()
+    os.chdir(src_path)
+
+    try:
+        return_code = __import__('pytest').main(['-q', '--doctest-modules'])
+    finally:
+        os.chdir(old_path)
+
+    if return_code == 0:
+        print("Congratulations. All tests have passed!")
+
+    return return_code
+
+
+__all__ = ['test']
