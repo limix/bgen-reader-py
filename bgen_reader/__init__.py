e7d6612a78f2a1716b85381b6f41dbe4d488c531
Danilo Horta <danilo.horta@gmail.com>
2017-08-30 10:18:21 +0100

    New setup, readme rst, requirements

--- bgen_reader/__init__.py
+++ bgen_reader/__init__.py
@@ -7,37 +7,19 @@ Bgen file format reader.
 
 """
 
-from __future__ import absolute_import as _absolute_import
+from __future__ import absolute_import as _
 
 import _cffi_backend
 
-from pkg_resources import DistributionNotFound as _DistributionNotFound
-from pkg_resources import get_distribution as _get_distribution
-
+from ._test import test
 from .bgen_reader import convert_to_dosage, read_bgen
 
-try:
-    __version__ = _get_distribution('bgen_reader').version
-except _DistributionNotFound:
-    __version__ = 'unknown'
+__name__ = "bgen-reader"
+__version__ = "0.1.17"
+__author__ = "Danilo Horta"
+__author_email__ = "horta@ebi.ac.uk"
 
-
-def test():
-    import os
-    p = __import__('bgen_reader').__path__[0]
-    src_path = os.path.abspath(p)
-    old_path = os.getcwd()
-    os.chdir(src_path)
-
-    try:
-        return_code = __import__('pytest').main(['-q', '--doctest-modules'])
-    finally:
-        os.chdir(old_path)
-
-    if return_code == 0:
-        print("Congratulations. All tests have passed!")
-
-    return return_code
-
-
-__all__ = ['test', 'read_bgen', 'convert_to_dosage']
+__all__ = [
+    "__name__", "__version__", "__author__", "__author_email__", "test",
+    'read_bgen', 'convert_to_dosage'
+]
--- setup.py
+++ setup.py
@@ -1,62 +1,109 @@
 from __future__ import unicode_literals
 
-import os
+import re
 import sys
+from os import chdir, getcwd
+from os.path import abspath, dirname, join
 
 from setuptools import find_packages, setup
 
 try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except (OSError, IOError, ImportError):
-    long_description = open('README.md').read()
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+PY2 = sys.version_info[0] == 2
+
+
+def _unicode_airlock(v):
+    if isinstance(v, bytes):
+        v = v.decode()
+    return v
+
+
+if PY2:
+    string_types = unicode, str
+else:
+    string_types = bytes, str
+
+
+class setup_folder(object):
+    def __init__(self):
+        self._old_path = None
+
+    def __enter__(self):
+        src_path = dirname(abspath(sys.argv[0]))
+        self._old_path = getcwd()
+        chdir(src_path)
+        sys.path.insert(0, src_path)
+
+    def __exit__(self, *_):
+        del sys.path[0]
+        chdir(self._old_path)
+
+
+def get_init_metadata(metadata, name):
+    expr = re.compile(r"__%s__ *=[^\"]*\"([^\"]*)\"" % name)
+    prjname = metadata['packages'][0]
+    data = open(join(prjname, "__init__.py")).read()
+    return re.search(expr, data).group(1)
+
+
+def make_list(metadata):
+    return metadata.strip().split('\n')
+
+
+def if_set_list(metadata, name):
+    if name in metadata:
+        metadata[name] = make_list(metadata[name])
+
+
+def set_long_description(metadata):
+    df = metadata['description_file']
+    metadata['long_description'] = open(df).read()
+    del metadata['description_file']
+
+
+def convert_types(metadata):
+    bools = ['True', 'False']
+    for k in metadata.keys():
+        v = _unicode_airlock(metadata[k])
+        if isinstance(metadata[k], string_types) and v in bools:
+            metadata[k] = v == 'True'
 
 
 def setup_package():
-    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    old_path = os.getcwd()
-    os.chdir(src_path)
-    sys.path.insert(0, src_path)
+    with setup_folder():
 
-    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-    pytest_runner = ['pytest-runner>=2.9'] if needs_pytest else []
+        config = ConfigParser()
+        config.read('setup.cfg')
 
-    setup_requires = ['cffi>=1.8'] + pytest_runner
-    install_requires = [
-        'cffi>=1.8', 'tqdm>=4.14', 'dask[array,bag,dataframe,delayed]>=0.15',
-        'scipy>=0.18', 'pandas>=0.19.2'
-    ]
-    tests_require = ['pytest', 'pytest-pep8']
+        metadata = dict(config.items('metadata'))
+        metadata['packages'] = find_packages()
+        metadata['platforms'] = eval(metadata['platforms'])
 
-    metadata = dict(
-        name='bgen-reader',
-        version='0.1.16',
-        maintainer="Danilo Horta",
-        maintainer_email="horta@ebi.ac.uk",
-        author="Danilo Horta",
-        author_email="horta@ebi.ac.uk",
-        license="MIT",
-        description="Bgen file format reader.",
-        long_description=long_description,
-        url='https://github.com/limix/bgen-reader-py',
-        packages=find_packages(),
-        zip_safe=False,
-        install_requires=install_requires,
-        setup_requires=setup_requires,
-        tests_require=tests_require,
-        include_package_data=True,
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ],
-        cffi_modules=["bgen_reader/_build.py:ffibuilder"])
+        metadata['version'] = get_init_metadata(metadata, 'version')
+        metadata['author'] = get_init_metadata(metadata, 'author')
+        metadata['author_email'] = get_init_metadata(metadata, 'author_email')
+        metadata['name'] = get_init_metadata(metadata, 'name')
 
-    try:
+        if_set_list(metadata, 'classifiers')
+        if_set_list(metadata, 'keywords')
+        if_set_list(metadata, 'cffi_modules')
+
+        if 'extras_require' in metadata:
+            metadata['extras_require'] = eval(metadata['extras_require'])
+
+        if 'console_scripts' in metadata:
+            metadata['entry_points'] = {
+                'console_scripts': make_list(metadata['console_scripts'])
+            }
+            del metadata['console_scripts']
+
+        set_long_description(metadata)
+        convert_types(metadata)
+
         setup(**metadata)
-    finally:
-        del sys.path[0]
-        os.chdir(old_path)
 
 
 if __name__ == '__main__':
