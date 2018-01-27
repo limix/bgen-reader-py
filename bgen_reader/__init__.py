d4a15716e3b371189656b29468cf09e2da14afb6
Danilo Horta <danilo.horta@gmail.com>
2018-01-27 11:37:35 +0000

    new setup

--- bgen_reader/__init__.py
+++ bgen_reader/__init__.py
@@ -9,17 +9,9 @@ Bgen file format reader.
 
 from __future__ import absolute_import as _
 
-import _cffi_backend as _
-
-from ._test import test
 from .bgen_reader import convert_to_dosage, read_bgen
+from .testit import test
 
-__name__ = "bgen-reader"
 __version__ = "1.0.1"
-__author__ = "Danilo Horta"
-__author_email__ = "horta@ebi.ac.uk"
 
-__all__ = [
-    "__name__", "__version__", "__author__", "__author_email__", "test",
-    'read_bgen', 'convert_to_dosage'
-]
+__all__ = ["__version__", "test", 'read_bgen', 'convert_to_dosage']
