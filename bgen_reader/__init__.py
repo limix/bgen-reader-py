117d96680e06313d519d186b3cba21bad8c58b28
Danilo Horta <danilo.horta@gmail.com>
2018-04-06 00:16:01 +0100

    remove pypandoc dependency

--- bgen_reader/__init__.py
+++ bgen_reader/__init__.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import as _
 from .bgen_reader import convert_to_dosage, read_bgen, create_metadata_file
 from .testit import test
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = ['__version__', 'test', 'read_bgen',
            'convert_to_dosage', 'create_metadata_file']
