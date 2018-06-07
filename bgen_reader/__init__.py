a5e3d9c30be122a6f04596d0489d20a22723d4e2
Danilo Horta <danilo.horta@gmail.com>
2018-06-07 17:47:38 +0100

    travis

--- bgen_reader/__init__.py
+++ bgen_reader/__init__.py
@@ -9,7 +9,7 @@ Bgen file format reader.
 
 from __future__ import absolute_import as _
 
-from .bgen_reader import convert_to_dosage, read_bgen, create_metadata_file
+from .bgen_reader import convert_to_dosage, create_metadata_file, read_bgen
 from .testit import test
 
 __version__ = "1.1.3"
