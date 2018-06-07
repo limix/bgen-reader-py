d9227b9dd7dfd0e91279491c02e4b9fe21db90ae
Danilo Horta <danilo.horta@gmail.com>
2018-06-07 16:52:28 +0100

    --codestyle

--- bgen_reader/testit.py
+++ bgen_reader/testit.py
@@ -12,7 +12,7 @@ def test(verbose=True):
         Exit code: ``0`` for success.
     """
 
-    args = ['--doctest-modules', '-x', '--pep8']
+    args = ['--doctest-modules', '-x', '--codestyle']
     if not verbose:
         args += ['--quiet']
 
