2b181effc1dac8f574619bcbb1f13039c472ebd0
Danilo Horta <danilo.horta@gmail.com>
2018-07-10 14:15:26 +0100

    bgen reader, wheel

--- bgen_reader/__init__.py
+++ bgen_reader/__init__.py
@@ -23,7 +23,7 @@ from ._metadata import create_metadata_file
 from ._testit import test
 from ._dosage import convert_to_dosage
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 __all__ = [
     '__version__', 'test', 'read_bgen', 'create_metadata_file',
