c342403dd0da0de112e38477bf0cdfbd6ab90eda
Danilo Horta <danilo.horta@gmail.com>
2019-01-13 02:40:20 +0000

    test

--- bgen_reader/interface.h
+++ bgen_reader/interface.h
@@ -37,23 +37,25 @@ void bgen_free_partition(struct bgen_vm *, int);
 int bgen_close_metafile(struct bgen_mf *);
 int bgen_ncombs(const struct bgen_vg *);
 int bgen_phased(const struct bgen_vg *);
-int bgen_missing(const struct bgen_vg *, size_t);
-int bgen_ploidy(const struct bgen_vg *, size_t);
+int bgen_missing(const struct bgen_vg *, int);
+int bgen_ploidy(const struct bgen_vg *, int);
 struct bgen_vg *bgen_open_genotype(struct bgen_file *, long);
 void bgen_close_genotype(struct bgen_vg *vg);
 int bgen_read_genotype(struct bgen_file *, struct bgen_vg *, double *);
 
 /* Deprecated functions. */
-int bgen_create_variants_metadata_file(const char *, const char *, int);
-struct bgen_var *bgen_read_variants_metadata(struct bgen_file *, struct bgen_vi **, int);
-int bgen_store_variants_metadata(const struct bgen_file *, struct bgen_var *,
-                                 struct bgen_vi *, const char *);
-struct bgen_var *bgen_load_variants_metadata(const struct bgen_file *, const char *,
-                                             struct bgen_vi **, int);
-void bgen_close_variant_genotype(struct bgen_vi *, struct bgen_vg *);
-void bgen_free_variants_metadata(const struct bgen_file *, struct bgen_var *);
-void bgen_free_index(struct bgen_vi *);
-struct bgen_vg *bgen_open_variant_genotype(struct bgen_vi *, size_t);
-int bgen_read_variant_genotype(struct bgen_vi *, struct bgen_vg *, double *);
-int bgen_sample_ids_presence(const struct bgen_file *);
-int bgen_max_nalleles(struct bgen_vi *);
+/* int bgen_create_variants_metadata_file(const char *, const char *, int); */
+/* struct bgen_var *bgen_read_variants_metadata(struct bgen_file *, struct bgen_vi **,
+ * int); */
+/* int bgen_store_variants_metadata(const struct bgen_file *, struct bgen_var *, */
+/*                                  struct bgen_vi *, const char *); */
+/* struct bgen_var *bgen_load_variants_metadata(const struct bgen_file *, const char *,
+ */
+/*                                              struct bgen_vi **, int); */
+/* void bgen_close_variant_genotype(struct bgen_vi *, struct bgen_vg *); */
+/* void bgen_free_variants_metadata(const struct bgen_file *, struct bgen_var *); */
+/* void bgen_free_index(struct bgen_vi *); */
+/* struct bgen_vg *bgen_open_variant_genotype(struct bgen_vi *, size_t); */
+/* int bgen_read_variant_genotype(struct bgen_vi *, struct bgen_vg *, double *); */
+/* int bgen_sample_ids_presence(const struct bgen_file *); */
+/* int bgen_max_nalleles(struct bgen_vi *); */
