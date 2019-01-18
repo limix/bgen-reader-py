e377ba6a0db87a71fdc3debd89dcaadb9050f1dc
Danilo Horta <danilo.horta@gmail.com>
2019-01-18 12:27:22 +0000

    doc to docs

--- docs/make.bat
+++ docs/make.bat
@@ -25,7 +25,7 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-cd .. && python setup.py develop & cd doc
+cd .. && python setup.py develop & cd docs
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
 
