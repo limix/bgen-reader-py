5ad4d5942f3bafb403bc3ad837e58d3d1494425d
Danilo Horta <danilo.horta@pm.me>
2020-04-07 13:30:15 +0100

    skip pypy

--- ci/before-build-win.bat
+++
@@ -1,6 +0,0 @@
-call ci\set-win-path.bat
-
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/horta/zlib.install/master/install.bat', 'install-zlib.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/horta/zstd.install/master/install.bat', 'install-zstd.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/horta/almosthere/master/install.bat', 'install-athr.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/limix/bgen/master/install.bat', 'install-bgen.bat')"
